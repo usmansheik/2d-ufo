571daa0f9f543fad408c1786edb16359a176c056
usmansheik <12105089@gift.edu.pk>
2016-10-29 12:03:34 +0500

    Game complete successfully

---
+++ 2D UFO Game/Assets/TutorialInfo/Scripts/playercontroller.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class playercontroller : MonoBehaviour {
+    public float speed;             //Floating point variable to store the player's movement speed.
+    public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
+    public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private int count;              //Integer to store the number of pickups collected so far.
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        count = 0;
+
+        winText.text = "";
+
+        SetCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+
+            other.gameObject.SetActive(false);
+
+        count = count + 1;
+
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+            winText.text = "You win!";
+    }
+}
---
+++ 2D UFO Game/Assets/TutorialInfo/Scripts/rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+
+	// Use this for initialization
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate thet transform of the game object this is attached to by 45 degrees, taking into account the time elapsed since last frame.
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+
+}
